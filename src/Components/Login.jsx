336569188997ab4165b22ab0d8429cb57099fe46
priyal.finestar <priyal@finestar.in>
2024-07-03 16:49:28 +0530

    create sigin and sigup form using react-hook-form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { login as authLogin } from '../Store/authSlice'
-import {Button, Input} from './index'
+import React, { useState } from 'react'
+import { Link, matchPath, useNavigate } from 'react-router-dom'
+import { Button, Input, Logo } from './index'
 import { useDispatch } from 'react-redux'
 import authService from '../appwirte/auth'
+import { useForm } from 'react-hook-form'
+import { login as authlogin } from '../Store/authSlice'
 const Login = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { register, handleSubmit } = useForm()
+    const [error, setError] = useState("")
+
+    const loginSubmit = async (data) => {
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if (session) {
+                const userData = await authService.getCurrentUser()
+                if (userData) dispatch(authlogin(userData));
+                navigate('/')
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
     return (
-        <div>Login</div>
+        <div className='flex items-center justify-center w-full'>
+            <div className='mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10'>
+                <Logo width={"100%"} />
+            </div>
+            <h2 className='text-center text-2xl font-bold leading-tight'>sign in to your account</h2>
+            <p className='mt-2 text-center text-base text-black/60 '>
+                Don&apos;t have any account?&nbsp;
+                <Link to="/signup"
+                    className='font-medium text-primary transition-all duration-200 hover:underline'
+                >
+                    Sign UP
+                </Link>
+            </p>
+            {error && <p className='text-red-600 mt-8 text-center'></p>}
+            <form onSubmit={handleSubmit(loginSubmit())} className='mt-8'>
+                <div>
+                    <Input
+                        label="Email: "
+                        placeHolder="Enter your email"
+                        type="email"
+                        {...register, "email", {
+                            required: true,
+                            validate: {
+                                matchPatten: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) ||
+                                    "Email address must be a valid address"
+
+                            }
+                        }}
+                    ></Input>
+                    <Input
+                        label="Password :"
+                        type="password"
+                        placeHolder="Enter your password"
+                        {...register, "passwor", {
+                            required: true,
+                        }}
+                    >
+                    </Input>
+                    <Button type='submit' className='w-full'>Sign in</Button>
+                </div>
+            </form>
+        </div>
     )
 }
 
---
+++ src/Components/Signup.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import authService from '../appwirte/auth'
+import { useNavigate, Link } from 'react-router-dom'
+import { login } from '../Store/authSlice'
+import { Button, Logon, Input, Logo } from './index'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+
+const Signup = () => {
+
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const [error, setError] = useState("")
+    const { register, handleSubmit } = useForm()
+
+    const create = async (data) => {
+        setError("")
+        try {
+            const userData = await authService.createAccount(data)
+            if (userData) {
+                const Data = await authService.getCurrentUser(userData)
+                if (Data) dispatch(login(Data))
+                navigate('/')
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+    return (
+        <div className='flex items-center justify-center w-full'>
+            <div className='mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10'>
+                <div className='mb-2 flex justify-center'>
+                    <span className='inline-block w-full max-w-[100]'>
+                        <Logo width={"100%"} />
+                    </span>
+                </div>
+                <h2 className='text-center text-2xl font-bold leading-tight'>Sign uo to create</h2>
+                <p className='mt-2 text-center text-base text-black/60 '>
+                    Already have an account?&nbsp;
+                    <Link to="/login"
+                        className='font-medium text-primary transition-all duration-200 hover:underline'
+                    >
+                        Sign In
+                    </Link>
+                </p>
+                {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+            </div>
+            <form onSubmit={handleSubmit(create)}>
+                <div className='space-y-5'>
+                    <Input
+                        label="Full Name:"
+                        placeplaceHolder="Enter your full name"
+                        {...register, "name", {
+                            required: true
+                        }}
+
+                    />
+                    <Input
+                        label="Email: "
+                        placeHolder="Enter your email"
+                        type="email"
+                        {...register, "email", {
+                            required: true,
+                            validate: {
+                                matchPatten: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) ||
+                                    "Email address must be a valid address"
+
+                            }
+                        }}
+                    />
+                    <Input
+                        label="Passwor:"
+                        placeHolder="Enter your password"
+                        type="password"
+                        {...register, "password", {
+                            required: true
+                        }}
+                    />
+                    <Button type="submit" className='w-full'>Create Account</Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
